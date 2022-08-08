32bbb6067c2747cd68355d9edbe1cc5f96b93cf5
Rohan <rohanmaheshrao10@gmail.com>
2022-08-08 16:32:30 +0530

    final push - working

--- ldr_soln.py
+++ ldr_soln.py
@@ -7,31 +7,33 @@ ldr_val = 0 # init value
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(rel, GPIO.OUT)
-GPIO.output(rel,GPIO.LOW)
+
+GPIO.output(rel,GPIO.HIGH)
 
 def rc_time(ldr):
-    count = 0
-    GPIO.setup(ldr,GPIO.OUT)
-    GPIO.output(ldr,GPIO.LOW)
-    time.sleep(0.1)
-    cur_time = time.time()#getting the current time
-    GPIO.setup(ldr,GPIO.IN)
+        count = 0
+        GPIO.setup(ldr,GPIO.OUT)
+        GPIO.output(ldr,GPIO.LOW)
+        time.sleep(0.1)
+        cur_time = time.time()#getting the current time
+        GPIO.setup(ldr,GPIO.IN)
+        while(GPIO.input(ldr) == GPIO.LOW):
+                count = time.time() - cur_time
 
-    while(GPIO.input(ldr) == GPIO.LOW):
-            count = time.time() - cur_time
-
-    return (count*1000)
+        return (count*1000)
 try:
-    while True:
+        while True:
 
-        ldr_val = int(rc_time(ldr))
-        print(ldr_val)
+                ldr_val = int(rc_time(ldr))
+                print(ldr_val)
 
-        if(ldr_val > 3):
-                GPIO.output(rel,GPIO.LOW)
+                if(ldr_val > 3):
+                        GPIO.output(rel,GPIO.HIGH)
 
-        else:
-                GPIO.output(rel,GPIO.HIGH)
+                if(ldr_val <= 3):
+                        GPIO.output(rel,GPIO.LOW)
+
 
 except KeyboardInterrupt:
-    GPIO.cleanup()
+        GPIO.cleanup()
+
